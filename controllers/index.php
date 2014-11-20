3afeb6046b6faad0e31e359e8020bae2eb67aac6
Red Hoang <red.hoang@miraclelinux.com>
2014-11-20 10:32:00 +0700

    -convert index to jplayer -update index_model.php to update mp3 source for index

--- controllers/index.php
+++ controllers/index.php
@@ -7,13 +7,48 @@ class Index extends Controller {
 	}
 	
 	function index() {
-		if(
+		if(USE_FLASH === '0'){
+			$listen ="";
+			if(isset($_GET['thuongthuc']))
+				$listen = $_GET['thuongthuc'];
+			switch ($listen){
+				case "westlife":
+				case "BB":
+				case "nhac-giao-huong":
+				case "happy-birthday":
+				case "nhac-mua":
+				case "loi-cua-song":
+				case "nhac-san":
+				case "my-gu":
+				case "bich-phuong":
+				case "guitar":
+				case "piano":
+				case "sao-tieu-dao":
+				case "nhac-au-mi":
+				case "tuoi-hoc-tro":
+				case "dong-que":
+				case "nhac-han":
+				case "rock":
+				case "rap":
+				case "anh-nho-em":
+				case "em-nho-anh":
+				case "top-viet":
+					break;
+				default:
+					$listen = "default";
+					break;
+			}
+			if(!$this->model)
+				$this->loadModel('index');
+			$this->view->music_album = $this->model->get_music_to_play($listen);
+		}
 		$this->view->render('index/index');
 	}
 
 	function updatedfhawnasdfzsxlaqwwerusjd(){
 		echo "Start update";
 		$this->model->update_music();
+		$this->model->update_music_index();
 		echo "Finish update";
 	}
 }
--- models/index_model.php
+++ models/index_model.php
@@ -1,15 +1,25 @@
 <?php
 
+require 'libs/Music/getlink.php';
 class Index_Model extends Model
 {
 	public function __construct()
 	{
 		parent::__construct();
 	}
+
+	function get_music_to_play($listen){
+		$sth = $this->db->prepare("SELECT value2 FROM ".DB_PREFIX."index
+					WHERE `key`=:listen");
+		$sth->execute(array(
+			':listen' => $listen
+		));
+		$data = $sth->fetchAll();
+		return $data[0][0];
+	}
 	
 	//function convert link music from table album to table music
 	function update_music(){
-		require 'libs/Music/getlink.php';
 		$sth = $this->db->prepare("SELECT id_album,music FROM ".DB_PREFIX."list_musics");
 		$sth->execute();
 		$data = $sth->fetchAll();
@@ -21,6 +31,22 @@ class Index_Model extends Model
 				':id_album' => $row['id_album'],
 				':music_embed' => $url_embed['data'],
 				':is_flash' => $url_embed['is_flash']
+			));
+		}
+	}
+
+	//function update_music_index()
+	function update_music_index(){
+		$sth = $this->db->prepare("SELECT id,value1 FROM ".DB_PREFIX."index");
+		$sth->execute();
+		$data = $sth->fetchAll();
+		foreach ($data as $row){
+			$url_embed = get_link_music_embed($row[1]);
+			$sthi = $this->db->prepare("UPDATE ".DB_PREFIX."index SET value2=:music_embed 
+				WHERE id=:id_album");
+			$sthi->execute(array(
+				':id_album' => $row[0],
+				':music_embed' => $url_embed['data'],
 			));
 		}
 	}
