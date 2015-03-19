e7f3498f70818be142f260f451b4627583e3730b
Red Hoang <vinh.hoang@miraclelinux.com>
2015-03-19 18:16:29 +0700

    add link_orig to table photo

--- controllers/abx.php
+++ controllers/abx.php
@@ -5,20 +5,20 @@ class Abx extends Controller {
 	function __construct() {
 		parent::__construct();
 	}
-	
+
 	function index() {
 		$userid = $this->checkPermission();
 		$this->view->username = $userid;
 		$this->view->userid = $userid;
 		$this->view->render('abx/index','header_mem');
 }
-	
+
 	function viewalbum(){
 		$userid = $this->checkPermission();
 		$this->view->username = $userid;
 		$this->view->userid = $userid;
 		//check if it's post edit request
-		if($_SERVER['REQUEST_METHOD'] == "POST"){ 
+		if($_SERVER['REQUEST_METHOD'] == "POST"){
 			if (isset($_POST['email'])) {
 				$email = $_POST['email'];
 				$this->view->data = $this->model->getListAlbumByEmail($email);
@@ -27,13 +27,13 @@ class Abx extends Controller {
 		$this->view->total = $this->model->getTotal($table_album = "list_albums");
 		$this->view->render('abx/viewalbum','header_mem');
 	}
-	
+
 	function viewuser(){
 		$userid = $this->checkPermission();
 		$this->view->username = $userid;
 		$this->view->userid = $userid;
 
-		if($_SERVER['REQUEST_METHOD'] == "POST"){ 
+		if($_SERVER['REQUEST_METHOD'] == "POST"){
 			if (isset($_POST['email'])) {
 				$email = $_POST['email'];
 				$this->view->data = $this->model->getUserByEmail($email);
@@ -50,12 +50,12 @@ class Abx extends Controller {
 		$this->view->data = $this->model->getListPublicAlbum();
 		$this->view->render('abx/viewalbum','header_mem');
 	}
-	
+
 	function previewstatus(){
 		$userid = $this->checkPermission();
-		
+
 		//check if it's post edit request
-		if($_SERVER['REQUEST_METHOD'] == "POST"){ 
+		if($_SERVER['REQUEST_METHOD'] == "POST"){
 			if (isset($_POST['idnumber'])) {
 				$id = $_POST['idnumber'];
 				$this->model->deletePreviewTable($id);
@@ -66,7 +66,7 @@ class Abx extends Controller {
 		$this->view->data = $this->model->getPreviewTable();
 		$this->view->render('abx/previewstatus','header_mem');
 	}
-	
+
 	function checkPermission() {
 		Session::init();
 		$userid = Session::get('userid');
@@ -78,23 +78,23 @@ class Abx extends Controller {
 		return $userid;
 	}
 
-	//function convert link music from table album to table music
-	function convert_table_album_to_music(){
-		require 'libs/Music/getlink.php';
-		$db = $this->model->db;
-		$sth = $db->prepare("SELECT id,music FROM ".DB_PREFIX."list_albums");
-		$sth->execute();
-		$data = $sth->fetchAll();
-		foreach ($data as $row){
-			$url_embed = get_link_music_embed($row['music']);
-			$sthi = $db->prepare("INSERT INTO ".DB_PREFIX."list_musics (`id_album`,`music`,`music_embed`,`is_flash`)
-				VALUES (:id_album,:music,:music_embed,:is_flash)");
-			$sthi->execute(array(
-				':id_album' => $row['id'],
-				':music' => $row['music'],
-				':music_embed' => $url_embed['data'],
-				':is_flash' => $url_embed['is_flash']
-			));
-		}
-	}
+	// //function convert link music from table album to table music
+	// function convert_table_album_to_music(){
+	// 	require 'libs/Music/getlink.php';
+	// 	$db = $this->model->db;
+	// 	$sth = $db->prepare("SELECT id,music FROM ".DB_PREFIX."list_albums");
+	// 	$sth->execute();
+	// 	$data = $sth->fetchAll();
+	// 	foreach ($data as $row){
+	// 		$url_embed = get_link_music_embed($row['music']);
+	// 		$sthi = $db->prepare("INSERT INTO ".DB_PREFIX."list_musics (`id_album`,`music`,`music_embed`,`is_flash`)
+	// 			VALUES (:id_album,:music,:music_embed,:is_flash)");
+	// 		$sthi->execute(array(
+	// 			':id_album' => $row['id'],
+	// 			':music' => $row['music'],
+	// 			':music_embed' => $url_embed['data'],
+	// 			':is_flash' => $url_embed['is_flash']
+	// 		));
+	// 	}
+	// }
 }
--- models/mem_model.php
+++ models/mem_model.php
@@ -7,12 +7,12 @@ class Mem_Model extends Model
 	{
             parent::__construct();
 	}
-        
-        
+
+
         //-----------------------get info user-------------------------
         function getuser($userid)
         {
-            $sth = $this->db->prepare("SELECT username FROM ".DB_PREFIX."users WHERE 
+            $sth = $this->db->prepare("SELECT username FROM ".DB_PREFIX."users WHERE
 				id = :id");
             $sth->execute(array(
                     ':id' => $userid
@@ -20,11 +20,11 @@ class Mem_Model extends Model
 
             $data = $sth->fetchAll();
             return $data[0]['username'];
-        } 
-        
+        }
+
         function getuserid($username)
         {
-            $sth = $this->db->prepare("SELECT id FROM ".DB_PREFIX."users WHERE 
+            $sth = $this->db->prepare("SELECT id FROM ".DB_PREFIX."users WHERE
 				username = :username");
             $sth->execute(array(
                     ':username' => $username
@@ -37,7 +37,7 @@ class Mem_Model extends Model
 		}
 
 		function getUserInfo($userid){
-			$sth = $this->db->prepare("SELECT username,email FROM ".DB_PREFIX."users WHERE 
+			$sth = $this->db->prepare("SELECT username,email FROM ".DB_PREFIX."users WHERE
 				id = :id");
             $sth->execute(array(
                     ':id' => $userid
@@ -46,10 +46,10 @@ class Mem_Model extends Model
             $data = $sth->fetchAll();
             return $data;
 		}
-        
+
         function getListAlbum($userid)
         {
-            $sth = $this->db->prepare("SELECT id,name_album,public,date FROM ".DB_PREFIX."list_albums WHERE 
+            $sth = $this->db->prepare("SELECT id,name_album,public,date FROM ".DB_PREFIX."list_albums WHERE
 				id_author = :id_author ORDER BY id DESC");
             $sth->execute(array(
                     ':id_author' => $userid
@@ -57,13 +57,13 @@ class Mem_Model extends Model
 
             $data = $sth->fetchAll();
             return $data;
-        }    
-        
+        }
+
         //-----------------------get info album-------------------------
         function getInfoAlbum($id_album)
         {
-			$sth = $this->db->prepare("SELECT alb.name_album,mu.music,mu.music_embed,st.title,st.content 
-				FROM ".DB_PREFIX."list_albums alb LEFT JOIN ".DB_PREFIX."list_musics mu ON alb.id = mu.id_album LEFT JOIN ".DB_PREFIX."story_album st ON alb.id = st.id_album 
+			$sth = $this->db->prepare("SELECT alb.name_album,mu.music,mu.music_embed,st.title,st.content
+				FROM ".DB_PREFIX."list_albums alb LEFT JOIN ".DB_PREFIX."list_musics mu ON alb.id = mu.id_album LEFT JOIN ".DB_PREFIX."story_album st ON alb.id = st.id_album
 				WHERE alb.id= :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album
@@ -72,10 +72,10 @@ class Mem_Model extends Model
             $data = $sth->fetchAll();
 			return $data[0];
 		}
-        
+
         function getNameAndMusicAlbum($id_album)
         {
-            $sth = $this->db->prepare("SELECT name_album,music FROM ".DB_PREFIX."list_albums WHERE 
+            $sth = $this->db->prepare("SELECT name_album,music FROM ".DB_PREFIX."list_albums WHERE
 				id = :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album
@@ -84,37 +84,37 @@ class Mem_Model extends Model
             $data = $sth->fetchAll();
             return $data;
         }
-        
+
         function getPhotosAlbum($id_album)
         {
-            $sth = $this->db->prepare("SELECT link,comment FROM ".DB_PREFIX."list_photos WHERE 
+            $sth = $this->db->prepare("SELECT link,comment FROM ".DB_PREFIX."list_photos WHERE
 				id_album = :id_album ORDER BY stt_photo ASC");
             $sth->execute(array(
                     ':id_album' => $id_album
             ));
-            
+
             $data = $sth->fetchAll();
             return $data;
         }
-        
+
         function getInfoPhotosAlbum($id_album)
         {
-            $sth = $this->db->prepare("SELECT id,link,comment FROM ".DB_PREFIX."list_photos WHERE 
+            $sth = $this->db->prepare("SELECT id,link_orig,comment FROM ".DB_PREFIX."list_photos WHERE
 				id_album = :id_album ORDER BY stt_photo ASC");
             $sth->execute(array(
                     ':id_album' => $id_album
             ));
-            
+
             $data = $sth->fetchAll();
             return $data;
         }
-        
-        
+
+
         //edit function
         function updateInfoAlbum($id_album,$name_album,$link_music)
         {
-            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_albums SET 
-				name_album = :name_album WHERE 
+            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_albums SET
+				name_album = :name_album WHERE
                                 id = :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album,
@@ -122,8 +122,8 @@ class Mem_Model extends Model
 				));
 
 			$url_embed = get_link_music_embed($link_music);
-			$sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_musics SET 
-				music = :music,music_embed = :music_embed, is_flash = :is_flash WHERE 
+			$sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_musics SET
+				music = :music,music_embed = :music_embed, is_flash = :is_flash WHERE
                                 id_album = :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album,
@@ -132,11 +132,11 @@ class Mem_Model extends Model
 					':is_flash' => $url_embed['is_flash']
 			));
         }
-        
+
         function updateStoryAlbum($id_album,$title_story,$content_story)
         {
-            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."story_album SET 
-				title = :title_story , content = :content_story WHERE 
+            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."story_album SET
+				title = :title_story , content = :content_story WHERE
                                 id_album = :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album,
@@ -144,48 +144,49 @@ class Mem_Model extends Model
                     ':content_story' => $content_story,
             ));
         }
-        
-        
-        function updatePhotoAlbum($id,$link,$cmt,$stt)
+
+
+        function updatePhotoAlbum($id,$link_orig,$cmt,$stt)
 		{
-			$link = $this->getURLPhoto($link);
+			$link = $this->getURLPhoto($link_orig);
 			$cmt = $this->normalizeStringSave($cmt);
-            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_photos SET 
-				link = :link , comment = :comment, stt_photo = :stt WHERE 
+            $sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_photos SET
+				link_orig = :link_orig ,link = :link , comment = :comment, stt_photo = :stt WHERE
                                 id = :id_photo");
             $sth->execute(array(
                     ':id_photo' => $id,
+                    ':link_orig' => $link_orig,
                     ':link' => $link,
                     ':comment' => $cmt,
                     ':stt' => $stt
             ));
         }
-        
+
         function removePhotoAlbum($id)
         {
-            $sth = $this->db->prepare("DELETE FROM ".DB_PREFIX."list_photos WHERE 
+            $sth = $this->db->prepare("DELETE FROM ".DB_PREFIX."list_photos WHERE
                                 id = :id_photo");
             $sth->execute(array(
                     ':id_photo' => $id
             ));
         }
-        
-        
+
+
         //--------------create new album---------------
         function createNewAlbum($id_user,$name_album)
         {
             //create new album in list album
-            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_albums 
+            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_albums
                             (`id_author` ,`name_album`) VALUES
                             (:id_author,:name_album)");
             $sth->execute(array(
                     ':id_author'     => $id_user,
                     ':name_album'    => $name_album
             ));
-            
+
             return $this->db->getInsertID();
         }
-        
+
         function createNewStoryAlbum($id_album,$title,$content)
         {
             //create new album in list album
@@ -197,16 +198,17 @@ class Mem_Model extends Model
                     ':content'         => $content,
             ));
         }
-        
-        function createNewPhotoAlbum($id_album,$link,$cmt,$stt)
+
+        function createNewPhotoAlbum($id_album,$link_orig,$cmt,$stt)
 		{
-			$link = $this->getURLPhoto($link);
+			$link = $this->getURLPhoto($link_orig);
 			$cmt = $this->normalizeStringSave($cmt);
-            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_photos 
-                            (`id_album` ,`stt_photo`,`link` ,`comment`) VALUES
-                            (:id_album,:stt,:link,:cmt)");
+            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_photos
+                            (`id_album` ,`stt_photo`,`link_orig`,`link` ,`comment`) VALUES
+                            (:id_album,:stt,:link_orig,:link,:cmt)");
             $sth->execute(array(
                     ':id_album'     => $id_album,
+                    ':link_orig'    => $link_orig,
                     ':link'    => $link,
                     ':cmt'         => $cmt,
                     ':stt' =>$stt
@@ -216,7 +218,7 @@ class Mem_Model extends Model
 		function createNewMusicAlbum($id_album,$linkmusic){
 			$url_embed = get_link_music_embed($linkmusic);
 			//create new album in list album
-			$sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_musics 
+			$sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."list_musics
 						(`id_album` ,`music`,`music_embed`,`is_flash`) VALUES
 						(:id_album,:music,:music_embed,:is_flash)");
 			$sth->execute(array(
@@ -226,8 +228,8 @@ class Mem_Model extends Model
 					':is_flash'     =>  $url_embed['is_flash']
 			));
 		}
-        
-        
+
+
         //delete album
         function deleteAlbum($id_album)
         {
@@ -238,7 +240,7 @@ class Mem_Model extends Model
                     ':id_album'     => $id_album
             ));
         }
-        
+
         function deleteStoryAlbum($id_album)
         {
             //create new album in list album
@@ -248,7 +250,7 @@ class Mem_Model extends Model
                     ':id_album'     => $id_album
             ));
         }
-        
+
         function deletePhotoyAlbum($id_album)
         {
             //create new album in list album
@@ -258,23 +260,23 @@ class Mem_Model extends Model
                     ':id_album'     => $id_album
             ));
         }
-        
-        
+
+
         function memGetIDPreview($userid){
-            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."preview 
+            $sth = $this->db->prepare("INSERT INTO ".DB_PREFIX."preview
                             (`author`) VALUES
                             (:author)");
             $sth->execute(array(
                     ':author'     => $userid
             ));
-            
+
             return $this->db->getInsertID();
         }
-        
+
 
 		// FB function
 		function publicFBPage($id_album){
-			$sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_albums SET 
+			$sth = $this->db->prepare("UPDATE ".DB_PREFIX."list_albums SET
 				public=1 WHERE id = :id_album");
             $sth->execute(array(
                     ':id_album' => $id_album
@@ -284,20 +286,20 @@ class Mem_Model extends Model
         //check
         function check_validation_with_album($user_name,$id_album)
         {
-            // $sth = $this->db->prepare("SELECT id FROM ".DB_PREFIX."list_albums WHERE 
+            // $sth = $this->db->prepare("SELECT id FROM ".DB_PREFIX."list_albums WHERE
 			// id = :id_album AND id_author = :id_author");
-			$sth = $this->db->prepare("SELECT u.username,alb.id FROM album_list_albums alb,album_users u WHERE 
+			$sth = $this->db->prepare("SELECT u.username,alb.id FROM album_list_albums alb,album_users u WHERE
 											alb.id_author=u.id and u.username=:username and alb.id=:id_album");
             $sth->execute(array(
                     ':id_album' => $id_album,
                     ':username' => $user_name
             ));
-            
+
             $count =  $sth->rowCount();
             return $count;
         }
-        
-		//function 
+
+		//function
 		function normalizeStringSave($cmt){
 			$cmt = str_replace("'","\'",$cmt);
 			return $cmt;
@@ -317,29 +319,29 @@ class Mem_Model extends Model
 
 		}
 
-        function _viewSource($url){ 
+        function _viewSource($url){
             $url = $this->nice_url($url);
-            $parse_url = parse_url($url); 
-            $headers = array("Host: {$parse_url['host']}"); 
+            $parse_url = parse_url($url);
+            $headers = array("Host: {$parse_url['host']}");
             ob_start();
-            $ch = curl_init(); 
-            curl_setopt($ch, CURLOPT_URL,$url); 
-            curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"); 
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
-            curl_setopt($ch, CURLOPT_REFERER, $url); 
-            curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate'); 
-            curl_setopt($ch, CURLOPT_HEADER, false); 
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-            $result = curl_exec($ch); 
-            curl_close($ch); 
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_REFERER, $url);
+            curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $result = curl_exec($ch);
+            curl_close($ch);
             ob_end_clean();
-            return $result; 
+            return $result;
 		}
 
         function getURLembed($url){
             $page = $this->_viewSource($url);
-            
+
             $pos = strpos($page,'<link rel="video_src"');
             if (false === $pos)
                 return $url;
@@ -350,7 +352,7 @@ class Mem_Model extends Model
             if ($pos_e === false)
                 return $url;
             $url_embed = substr($page,$pos,$pos_e - $pos);
-            
+
             return $url_embed;
         }
 
